d82fb06c892c17a8505b700b89d2e73adfa4ee98
Sarthak Singh <105309861+sarthakk1890@users.noreply.github.com>
2024-01-05 15:06:31 +0530

    Update salesController.js

--- backend/controllers/salesController.js
+++ backend/controllers/salesController.js
@@ -236,7 +236,8 @@ exports.partyCreditHistory = catchAsyncErrors(async (req, res, next) => {
   const id = req.params.id;
   const data = await SalesOrder.find({
     party: id
-  }).sort({ createdAt: -1 });
+  }).sort({ createdAt: -1 })
+    .populate('party');
 
   data.map((value, idx) => {
     if (!value.modeOfPayment[0].mode) {
