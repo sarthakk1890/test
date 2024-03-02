c986d333bf2d26609db227e07e07394a9d32c4e1
sarthakk1890 <144singhsarthak@gmail.com>
2024-03-02 13:54:14 +0530

    Fixed report

--- backend/controllers/reportController.js
+++ backend/controllers/reportController.js
@@ -36,6 +36,7 @@ exports.getReportofUser = catchAsyncErrors(async (req, res, next) => {
       {
         path: "orderItems",
         populate: { path: "product", model: InventoryModel },
+        populate: { path: "membership", select: "plan validity sellingPrice basePrice GSTincluded GSTRate CGST SGST IGST membershipType" }
       },
       "party",
       { path: "user", select: "taxFile" },
@@ -52,7 +53,7 @@ exports.getReportofUser = catchAsyncErrors(async (req, res, next) => {
     res.status(200).json({
       success: true,
       sales,
-      
+
     });
 
   }
@@ -81,7 +82,7 @@ exports.getReportofUser = catchAsyncErrors(async (req, res, next) => {
     res.status(200).json({
       success: true,
       purchase,
-      
+
     });
   }
 
@@ -95,7 +96,7 @@ exports.getReportofUser = catchAsyncErrors(async (req, res, next) => {
     res.status(200).json({
       success: true,
       expense,
-      
+
     });
   }
 
@@ -115,7 +116,7 @@ exports.getReportofUser = catchAsyncErrors(async (req, res, next) => {
     res.status(200).json({
       success: true,
       sales,
-      
+
     });
   }
 
@@ -128,7 +129,7 @@ exports.getReportofUser = catchAsyncErrors(async (req, res, next) => {
     res.status(200).json({
       success: true,
       inventories,
-      
+
     });
   }
 
@@ -142,7 +143,7 @@ exports.getReportofUser = catchAsyncErrors(async (req, res, next) => {
       success: true,
       count: estimates.length,
       estimates,
-      
+
     });
   }
 
