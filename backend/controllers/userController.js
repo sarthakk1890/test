73add62c34262435a39794402080cfc5060b9d17
sarthakk1890 <144singhsarthak@gmail.com>
2024-03-13 19:12:13 +0530

    Updated User.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -406,7 +406,16 @@ exports.sendOtp = catchAsyncErrors(async (req, res, next) => {
 });
 
 exports.refreshJwtToken = catchAsyncErrors(async (req, res, next) => {
-  const { token } = req.cookies;
+  let token;
+
+  // Check if token exists in cookies
+  if (req.cookies.token) {
+    token = req.cookies.token;
+  }
+
+  if (!token && req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
   if (!token) {
     return next(new ErrorHandler("Please login to access this resource", 401));
   }
