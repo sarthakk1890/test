81a94437dfa608c17886a3769609e67303c1fa37
Sarthak Singh <105309861+sarthakk1890@users.noreply.github.com>
2024-04-05 15:57:09 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ const corsOptions = {
   origin: "*", // Allow requests from any origin
   credentials: true,
   methods: "GET,PUT,POST,DELETE,OPTIONS",
-  allowedHeaders: "Content-Type, Authorization, Content-Length, X-Requested-With",
+  allowedHeaders: "Content-Type, Authorization, Authorization_subuser, Content-Length, X-Requested-With",
 };
 app.use(cors(corsOptions));
 
@@ -250,7 +250,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With"
+    "Content-Type, Authorization, Authorization_subuser, Content-Length, X-Requested-With"
   );
   next();
 });
